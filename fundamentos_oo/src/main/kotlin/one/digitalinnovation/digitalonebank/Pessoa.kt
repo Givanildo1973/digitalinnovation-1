7e8a0653045e7de7e99a3cf5fee1c8685eeca1f8
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-03 14:07:53 -0300

    inclusao de dados

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digitalonebank
+
+class Pessoa {
+    var nome: String = "Givanildo"
+    var  cpf: String = "131.510.508-08"
+
+}
+fun main(){
+    val Givanildo = Pessoa()
+
+
+    println(Givanildo.nome)
+    println(Givanildo.cpf)
+}
