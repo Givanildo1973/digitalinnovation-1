971553c781395583dda5c9a5f2cc5decfe33b926
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-04 15:54:00 -0300

    Inclusão do Private

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,11 +2,14 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Givanildo"
+
     var  cpf: String = "123.123.123-11"
+    private set
 }
 fun main () {
     val Givanildo = Pessoa()
 
+    println(Givanildo)
     println(Givanildo.nome)
     println(Givanildo.cpf)
 
