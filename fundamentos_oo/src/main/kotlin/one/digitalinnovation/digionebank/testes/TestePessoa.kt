665935d3451803724e72ec016bf72ecaf3bef3db
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-04 19:59:45 -0300

    Criacao de teste

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/testes/TestePessoa.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Pessoa
+
+fun main() {
+    val Givanildo = Pessoa(nome = "Givanildo Santos", cpf = "13151050808")
+
+    println(Givanildo.nome)
+    print(Givanildo.cpf)
+}
