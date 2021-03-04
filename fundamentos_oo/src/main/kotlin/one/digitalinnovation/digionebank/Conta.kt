fe5104a3546b8c82e3da1e4f732a6799d32b5cec
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-04 16:40:37 -0300

    Criação da classe da conta

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -1,28 +1,12 @@
 package one.digitalinnovation.digionebank
 
-import java.lang.IllegalArgumentException
-
-class Conta(
+class Conta {
     val agencia: String,
-    val numero: String
-) : Depositavel, Sacavel {
-    var saldo: Double = 0.0; private set
-
-    override fun deposito(valor: Double) {
-        this.saldo += valor
+    val numero: String,
+    val saldo: Int
+} {
+    fun deposito (valor: Int){
     }
-
-    override fun saque(valor: Double) {
-        if(this.saldo < valor) throw IllegalArgumentException("Não é possível fazer saque de valor maior que saldo!")
-
-        this.saldo -= valor
-    }
-
-    override fun toString(): String {
-        return """
-            Ag:     $agencia
-            Cc:     $numero
-            Saldo:  $saldo
-        """.trimIndent()
+    fun saque (valor: Int) {
     }
 }
