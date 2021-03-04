6dc82ec4b1cca7777962f26488dccd29595b76dc
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-04 08:48:14 -0300

    Criado classe de conta

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Conta.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation
+
+class Conta {
+    val agencia: String,
+    val  numero: String,
+    val saldo: Int
+}{
+    fun deposito (valor: Int){
+}
+    fun saque (valor: Int){
+}}
