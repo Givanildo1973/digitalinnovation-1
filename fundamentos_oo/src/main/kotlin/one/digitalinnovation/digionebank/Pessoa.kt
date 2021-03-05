c003538bc7fbcf8a8cf810444b4efbd85373e621
Givanildo1973 <givanildosantosdonascimento@hotmail.com>
2021-03-04 21:41:21 -0300

    Criacao Enum cliente a teste para IT

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Givanildo"
-
-    var  cpf: String = "123.123.123-11"
-        private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-fun main () {
-    val Givanildo = Pessoa()
-
-    println(Givanildo.pessoaInfo())
+abstract class Pessoa{
+    val nome: String,
+    val  cpf: String
 }
